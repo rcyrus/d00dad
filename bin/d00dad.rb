d61a608a69a325a0af637e68d72b0eb60e3689b2
Rajeev Cyrus <rcyrus@gmail.com>
2017-02-15 22:26:02 -0600

    handle errors with tests

--- bin/d00dad.rb
+++ bin/d00dad.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 
 require 'thor'
+require 'tty-command'
 
 class D00dad < Thor
 
+  desc 'branches GIT_DIR', 'Show both local and remote branches from GIT_DIR'
+
+  def branches(git_dir = '.')
+    cmd = TTY::Command.new(printer: :quiet, color: true)
+    cmd.run('git branch -a', chdir: git_dir)
+  rescue TTY::Command::ExitError => e
+    exit 1
+  end
 end
 
 D00dad.start(ARGV)
--- spec/d00dad_spec.rb
+++ spec/d00dad_spec.rb
@@ -2,5 +2,34 @@ require 'spec_helper'
 
 RSpec.describe 'First Run', :type => :aruba do
 
-  it { expect(run('d00dad.rb')).to have_output(/Commands/) }
+  context 'When run without command' do
+    before(:each) { run('d00dad.rb') }
+    before(:each) { stop_all_commands }
+
+    it 'should print help information' do
+      expect(extract_text(unescape_text(last_command_started.output))).to match(/Commands/)
+    end
+
+    it 'should run successfully' do
+      expect(last_command_started).to be_successfully_executed
+    end
+  end
+
+  context 'When running branches command' do
+
+    it 'should show a listing of branches' do
+      expect(run('d00dad.rb branches')).to have_output(/develop/)
+      expect(last_command_started).to be_successfully_executed
+    end
+
+    it 'displays error for bad directory' do
+      expect(run('d00dad.rb branches xyz')).to have_output(/No such file or directory/)
+      expect(last_command_started).to_not be_successfully_executed
+    end
+
+    it 'displays error for non git directory' do
+      expect(run('d00dad.rb branches /')).to have_output(/Not a git repository/)
+      expect(last_command_started).to_not be_successfully_executed
+    end
+  end
 end
